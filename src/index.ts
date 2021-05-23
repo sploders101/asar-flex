4aa9ebe7597989c5a7ee0131af6b4ffbe99c9d41
Shaun Keys <mariobuddy@gmail.com>
2021-05-22 18:17:20 -0600

    Export reader interfaces from index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,11 @@ export {
 	AsarGetterStream,
 } from "./reader";
 export {
+	default as AsarReader,
+} from "./reader/base";
+export {
+	default as AsarReaderStreams,
+} from "./reader/streams";
+export {
 	AsarWriter,
 } from "./writer";
